79e43f0b7e320a472f47b28f2604cef3c180676f
John Charles Woods <woods.johnny.charles@gmail.com>
2025-10-30 20:27:00 -0400

    Add Code of Conduct and create SpecialPageSetup class

---
+++ includes/SpecialPageSetup.php
@@ -0,0 +1,5 @@
+<?php
+namespace PHPizza;
+class SpecialPageSetup extends SpecialPage {
+    
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'johnnycharlesw-github/phpizza',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '74b8a3921d6982c7409b7d2a25144c23cd64cba1',
+        'reference' => 'cd4ea091dfa522cb39499eaabe8933f2ac8d5a14',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'johnnycharlesw-github/phpizza' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '74b8a3921d6982c7409b7d2a25144c23cd64cba1',
+            'reference' => 'cd4ea091dfa522cb39499eaabe8933f2ac8d5a14',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

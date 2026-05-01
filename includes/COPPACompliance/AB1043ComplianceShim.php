16ab736191758a3af046355d56539c7ee9274251
John Charles Woods <woods.johnny.charles@gmail.com>
2026-04-30 21:42:40 -0400

    Started working on AB1043 compliance

---
+++ includes/COPPACompliance/AB1043ComplianceShim.php
@@ -0,0 +1,32 @@
+<?php
+namespace PHPizza\COPPACompliance;
+
+use PHPizza\UserManagement\User;
+use PHPizza\UserManagement\UserDatabase;
+
+class AB1043ComplianceShim {
+    const YES = true;
+    const NO = false;
+
+    private $userdb;
+
+    public function __construct()
+    {
+        global $dbServer, $dbUser, $dbPassword, $dbName, $dbType;
+        $this->userdb = new UserDatabase($dbServer, $dbUser, $dbPassword, $dbName, $dbType);
+    }
+
+    public function _doAgeVerification(User $minor): int {
+        if (!empty($verifyAge)) {
+            $ageDatetime = $minor->when_was_I_born();
+            $ageGroup = date_format($ageDatetime, "YY");
+            return $ageGroup;
+        }
+        return 18;
+    }
+
+    public function ageVerify(string $minor_username): int {
+        $user = $this->userdb->get_user_by_username($minor_username);
+        return $this->_doAgeVerification($user);
+    }
+}
